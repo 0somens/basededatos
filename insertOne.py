a8efa6da50fb3f6e7981e14202766a1f7d8b808e
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-05-22 17:12:42 -0400

    asdasdas
    asdasd

---
+++ insertOne.py
@@ -0,0 +1,20 @@
+from coneccion import *
+
+db.cliente.Supermercado
+coleccion = db.productos
+
+nombre = "Matias Osores"
+direccion = "psj x 321"
+telefono = "+569 3456 4321"
+correo_electronico = "matias.osores@inacapmail.cl"
+
+documentos = {
+	"nombre": "Matias O",
+	"direccion":"psj imaginario 123",
+	"telefono": "+569 1234 4321",
+	"correo_electronico":"mati.osores@gmail.com"
+      } 
+
+
+
+      #Tarea Hacer el update y el delete!!!
