550b190d11a64ea23cea2b5108f1a809e9829439
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-06-05 16:58:02 -0400

    funciona
    Ahora sin con menos campos a completar en la base de datos "Supermercado" y en  la coleccion "proveedores"

--- index.py
+++ index.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 def home():
     db = conexion()
-    lista = db.proveedores
-    datos = lista.find()
+    proveedores = db.proveedores
+    datos = proveedores.find()
 
-    return render_template('index.html', lista = datos) #Esto manda a llamar a la /templates/index.html para que una vez cargado aparezca en pantalla 
+    return render_template('index.html', proveedores = datos) #Esto manda a llamar a la /templates/index.html para que una vez cargado aparezca en pantalla 
 if __name__ == '__main__':
     app.run(debug=True) #Ejecuta la aplicacion del Main en modo de prueba (mientras el servidor este activo puedo modificar y puedo ver lo cambios de manera sincronica )
