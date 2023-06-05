e0b8031add59743ff7ab8f9f929775e891539847
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-06-05 17:30:20 -0400

    saved
    saved

--- index.py
+++ index.py
@@ -10,7 +10,13 @@ def home():
     db = conexion()
     proveedores = db.proveedores
     datos = proveedores.find()
+    
+    clientes = db.clientes
+    datosclientes = clientes.find()
 
-    return render_template('index.html', proveedores = datos) #Esto manda a llamar a la /templates/index.html para que una vez cargado aparezca en pantalla 
+    productos = db.productos
+    datosproductos = productos.find()
+
+    return render_template('index.html', proveedores = datos , clientes = datosclientes, productos = datosproductos) #Esto manda a llamar a la /templates/index.html para que una vez cargado aparezca en pantalla 
 if __name__ == '__main__':
     app.run(debug=True) #Ejecuta la aplicacion del Main en modo de prueba (mientras el servidor este activo puedo modificar y puedo ver lo cambios de manera sincronica )
